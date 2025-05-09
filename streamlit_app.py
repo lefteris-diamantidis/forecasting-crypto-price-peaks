5470b5a8df9389426593e14259363d00dff69747
lefteris-diamantidis <lkarathan12@gmail.com>
2025-05-09 18:00:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,7 @@ with st.expander("Data"):
   df_btc = pd.read_csv("https://raw.githubusercontent.com/lefteris-diamantidis/forecasting-crypto-price-peaks/refs/heads/main/data/final_data_btc.csv")
   st.write("PAXG Data")
   df_paxg = pd.read_csv("https://raw.githubusercontent.com/lefteris-diamantidis/forecasting-crypto-price-peaks/refs/heads/main/data/final_data_paxg.csv")
+  st.write("ETH Data")
   df_eth = pd.read_csv("https://raw.githubusercontent.com/lefteris-diamantidis/forecasting-crypto-price-peaks/refs/heads/main/data/final_data_eth.csv")
+  st.write("BNB Data")
   df_bnb = pd.read_csv("https://raw.githubusercontent.com/lefteris-diamantidis/forecasting-crypto-price-peaks/refs/heads/main/data/final_data_bnb.csv")
