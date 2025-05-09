dc2a225f12301127a307200716aa1bb0cb1a33e5
lefteris-diamantidis <lkarathan12@gmail.com>
2025-05-09 10:36:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title("Forecasting TUNH App")
 st.info("This is an app that Forecasts Cryptos TUNH")
 
 with st.expander("Data"):
-  st.write("Raw Data")
-  df_btc = pd.read_csv("final_data_btc.csv")
+  st.write("BTC Data")
+  df_btc = pd.read_csv("https://github.com/lefteris-diamantidis/forecasting-crypto-price-peaks/blob/main/final_data_btc.csv")
   df
