f8edf2fe05810816c73a83a26da45ba5e6f0b38c
lefteris-diamantidis <lkarathan12@gmail.com>
2025-05-09 10:34:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-
-
-
 st.title("Forecasting TUNH App")
-
 st.info("This is an app that Forecasts Cryptos TUNH")
-df_btc = pd.read_csv("final_data_btc.csv")
-df.head()
+
+with st.expander("Data"):
+  st.write("Raw Data")
+  df_btc = pd.read_csv("final_data_btc.csv")
+  df.head()
